a043ea1113c4a7ff01f247d7d5bed3f47473f6a3
shubhamsoni3005 <110595082+shubhamsoni3005@users.noreply.github.com>
2022-11-11 23:21:53 +0530

    Add files via upload

---
+++ ques4.java
@@ -0,0 +1,10 @@
+public class ques4 {
+    public static void main(String[] args)
+    {
+        System.out.println(24);//output
+        /*
+        a is static. It will be incremented 3 times when object constructor
+        is called. It is incremented 4th time when obj3.m1() is called.
+         */
+    }
+}
---
+++ ques5.java
@@ -0,0 +1,9 @@
+public class ques5 {
+    public static void main(String[] args){
+        System.out.println(10); //output
+        /*
+        x is static. the static blocked will be called when class is loaded
+        and x is stored as 10. It is printed when System.out is used.
+         */
+    }
+}
